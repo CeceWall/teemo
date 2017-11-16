25d8920e0f9314d228778705f766db9f4aec9213
guoweijian <guoweijian@terminus.io>
2017-11-16 17:50:19 +0800

    fix: 修复eslint失败

--- src/components/FastPrice/PriceTable.jsx
+++ src/components/FastPrice/PriceTable.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 class PriceTable extends React.Component {
 
-  componentDidMount() {
-    const {location, history} = this.props;
-    console.log(location);
-    if (!location.state) {
-      history.replace('/fast-price')
+    componentDidMount() {
+        const {location, history} = this.props;
+        console.log(location);
+        if (!location.state) {
+            history.replace('/fast-price')
+        }
     }
-  }
 
-  render() {
-    return (
-      <div>
+    render() {
+        return (
+            <div>
         bla
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 export default PriceTable;
